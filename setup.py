847bdd5fb9402d9e85181885328841c56d2c1f06
margulies <daniel.margulies@gmail.com>
2015-10-23 18:25:32 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'surfdist',
+  packages = ['surfdist'],
+  version = '0.1',
+  description = 'For calculating exact geodesic distances on cortical surface meshes',
+  author = 'Daniel Margulies',
+  author_email = 'margulies@cbs.mpg.de',
+  url = 'https://github.com/margulies/surfdist',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+  keywords = ['geodesic', 'distance', 'brain', 'cortex'],
+  classifiers = [],
+)
