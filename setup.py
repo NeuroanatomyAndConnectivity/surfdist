f8026f6cc27aa069d186692e38107b7acca7061d
margulies <daniel.margulies@gmail.com>
2015-10-23 23:18:11 -0500

    fixed package

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
 setup(
   name = 'surfdist',
-  packages = ['surfdist'],
-  version = '0.1',
+  packages = ['surfdist/surfdist'],
+  install_requires = ['numpy',                                              
+                      'mayavi',                        
+                      'gdist'],
+  version = '0.12',
   description = 'For calculating exact geodesic distances on cortical surface meshes',
   author = 'Daniel Margulies',
   author_email = 'margulies@cbs.mpg.de',
   url = 'https://github.com/margulies/surfdist',
-  download_url = 'https://github.com/margulies/surfdist/tarball/0.1',
+  download_url = 'https://github.com/margulies/surfdist/tarball/0.12',
   keywords = ['geodesic', 'distance', 'brain', 'cortex'],
   classifiers = [],
 )
