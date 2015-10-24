738ebe6def099e0761521ba2752d0620dde314b4
margulies <daniel.margulies@gmail.com>
2015-10-24 00:14:54 -0500

    additions to demo and functions

--- examples/demo.py
+++ examples/demo.py
@@ -4,18 +4,19 @@ import os
 from mayavi import mlab
 import surfdist.surfdist as sd
 
-label_text = 'S_central'
 base_dir = '/Applications/freesurfer/subjects/fsaverage4/'
 
 surf = nib.freesurfer.read_geometry(os.path.join(base_dir, 'surf/lh.pial'))
 cort = np.sort(nib.freesurfer.read_label(os.path.join(base_dir, 'label/lh.cortex.label')))
 annot = nib.freesurfer.read_annot(os.path.join(base_dir, 'label/lh.aparc.a2009s.annot'))
 
+# Calculate geodesic distance from central sulcus:
+label_text = 'S_central'
 label_ind = annot[2].index(label_text)
 labels = np.where(np.in1d(annot[0],label_ind))
 labels = np.where(np.in1d(cort, labels))[0]
 src = np.array(labels, dtype=np.int32)
 
-dist = sd.distcalc(surf, cort, src)
+dist = sd.dist_calc(surf, cort, src)
 data = sd.recort(dist, surf, cort)
 sd.viz(surf, data)
--- surfdist/surfdist.py
+++ surfdist/surfdist.py
@@ -1,38 +1,68 @@
-# -*- coding: utf-8 -*-
-
 import numpy as np
 
+
 def surf_keep_cortex(surf, cortex):
-        
+    """
+    Remove medial wall from cortical surface to ensure that shortest paths are only calculated through the cortex.
+    """
     vertices = np.array(surf[0][cortex], dtype=np.float64)
 
     keep = np.zeros(len(surf[1]))
-    for i in [0,1,2]:
-        keep += np.array([item in cortex for item in surf[1][:,i]])
+    for i in [0, 1, 2]:
+        keep += np.array([item in cortex for item in surf[1][:, i]])
     ind = np.where(keep == 3)
     triangles = np.array(surf[1][ind], dtype=np.int32)
-    for c,i in enumerate(cortex):
+    for c, i in enumerate(cortex):
         triangles[np.where(triangles == i)] = c
-    
+
     return vertices, triangles
 
+
 def recort(input_data, surf, cortex):
+    """
+    Return data values to space of full cortex (including medial wall), with medial wall equal to zero.
+    """
     data = np.zeros(len(surf[0]))
     data[cortex] = input_data
     return data
-    
+
+
 def viz(surf, data):
+    """
+    Visualize results on cortical surace using mayavi.
+    """
     vertices = np.array(surf[0], dtype=np.float64)
     triangles = np.array(surf[1], dtype=np.int32)
     from mayavi import mlab
-    mlab.triangular_mesh(vertices[:,0], vertices[:,1], vertices[:,2], triangles, scalars=data)    
+    mlab.triangular_mesh(
+        vertices[:, 0], vertices[:, 1], vertices[:, 2], triangles, scalars=data)
 
-def distcalc(surf, cortex, src):
-    
+
+def dist_calc(surf, cortex, src):
+    """
+    Calculate exact geodesic distance along cortical surface from set of source nodes.
+    """
     import gdist
 
     vertices, triangles = surf_keep_cortex(surf, cortex)
-    dist = gdist.compute_gdist(vertices, triangles, source_indices = src)
+    dist = gdist.compute_gdist(vertices, triangles, source_indices=src)
 
     return dist
-    
+
+
+def zone_calc(surf, cortex, src):
+    """
+    Calculate closest nodes to each source node using exact geodesic distance along the cortical surface.
+    """
+    import gdist
+
+    vertices, triangles = surf_keep_cortex(surf, cortex)
+
+    dist_vals = []
+    for x in range(len(src)):
+        dist_vals.append(
+            gdist.compute_gdist(vertices, triangles, source_indices=src[x]))
+
+    zone = argsort(dist_vals)
+
+    return zone
