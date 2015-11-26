1863c0df2a3162ac73345748308e20eabe19e384
margulies <daniel.margulies@gmail.com>
2015-11-26 02:33:52 +0100

    fixed mistake with secondary and unnecessary translation of source points

--- surfdist/surfdist.py
+++ surfdist/surfdist.py
@@ -9,8 +9,7 @@ def dist_calc(surf, cortex, src):
     from utils import surf_keep_cortex, translate_src, recort
 
     vertices, triangles = surf_keep_cortex(surf, cortex)
-    src_new = translate_src(src, cortex)
-    data = gdist.compute_gdist(vertices, triangles, source_indices=src_new)
+    data = gdist.compute_gdist(vertices, triangles, source_indices=src)
     dist = recort(data, surf, cortex)
     del data
 
@@ -22,17 +21,19 @@ def zone_calc(surf, cortex, src):
     Calculate closest nodes to each source node using exact geodesic distance along the cortical surface.
     """
     import gdist
-    from utils import surf_keep_cortex, recort
+    from utils import surf_keep_cortex, translate_src, recort
 
     vertices, triangles = surf_keep_cortex(surf, cortex)
 
     dist_vals = np.zeros((len(src), len(vertices)))
-    for x in range(len(src)):
-        dist_vals[x, :] = gdist.compute_gdist(vertices, triangles,
-                                              source_indices=np.array([src[x]], dtype=np.int32))
 
-    data = np.argsort(dist_vals, axis=0)[-1, :] + 1
+    for x in range(len(src)):
+        dist_vals[x, :] = gdist.compute_gdist(vertices, triangles, source_indices=src[x])
+
+    data = np.argsort(dist_vals, axis=0)[0, :] + 1
+
     zone = recort(data, surf, cortex)
+
     del data
 
     return zone
