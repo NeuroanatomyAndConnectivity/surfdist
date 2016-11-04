e34d318a1c745b7a5b60c586cd64ba71fa670257
David Ellis <david.ellis@unmc.edu>
2016-11-04 13:15:01 -0500

    STYLE: adds documentation on load_freesurfer_label inputs

--- surfdist/load.py
+++ surfdist/load.py
@@ -4,15 +4,21 @@ import numpy as np
 
 def load_freesurfer_label(annot_input, label_name, cortex=None):
     """
-    Get source node list for freesurfer label.
+    Get source node list for a specified freesurfer label.
+
+    Inputs
+    -------
+    annot_input : freesurfer annotation label file
+    label_name : freesurfer label name
+    cortex : not used
     """
 
     if cortex:
         print("Warning: cortex is not used to load the freesurfer label")
 
     labels, color_table, names = nib.freesurfer.read_annot(annot_input)
-    label_index = names.index(label_name)
-    label_nodes = np.array(np.where(np.in1d(labels, label_index)), dtype=np.int32)
+    label_value = names.index(label_name)
+    label_nodes = np.array(np.where(np.in1d(labels, label_value)), dtype=np.int32)
 
     return label_nodes
 
