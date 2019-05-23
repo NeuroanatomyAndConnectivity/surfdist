39a8d80b7a7233e58f2ef7feff6eebef20d7dde1
margulies <daniel.margulies@gmail.com>
2019-05-23 16:17:20 +0200

    updated installation scripts

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
-from distutils.core import setup
+from setuptools import setup
+
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
 setup(
   name = 'surfdist',
   packages = ['surfdist'],
-  install_requires = ['numpy',
-                      'gdist',
-                      'nibabel',
-                      'scipy',
-                      'numba'],
   version = '0.14.0',
   description = 'For calculating exact geodesic distances on cortical surface meshes',
   author = 'Daniel Margulies',
   author_email = 'daniel.margulies@gmail.com',
   url = 'https://github.com/NeuroanatomyAndConnectivity/surfdist',
-  download_url = 'https://github.com/NeuroanatomyAndConnectivity/surfdist/tarball/0.14.0',
   keywords = ['geodesic', 'distance', 'brain', 'cortex'],
-  classifiers = [],
+  license='LICENSE.txt',
+  long_description=open('README.md').read(),
+  install_requires=load_requirements("requirements.txt")
 )
