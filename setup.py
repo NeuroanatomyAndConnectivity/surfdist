a61f0cb4c2f7d455da208d007798b66fc0f48aa9
margulies <daniel.margulies@gmail.com>
2020-07-28 16:38:38 +0300

    added reqs

--- setup.py
+++ setup.py
@@ -2,14 +2,8 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-    
-try:
-    # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:
-    # for pip <= 9.0.3
-    from pip.req import parse_requirements
 
+from pip._internal.req import parse_requirements
 def load_requirements(fname):
     reqs = parse_requirements(fname, session="test")
     return [str(ir.req) for ir in reqs]
@@ -17,7 +11,7 @@ def load_requirements(fname):
 setuptools.setup(
   name = 'surfdist',
   packages = ['surfdist'],
-  version = '0.15.4',
+  version = '0.15.5',
   description = 'For calculating exact geodesic distances on cortical surface meshes',
   long_description=long_description,
   long_description_content_type="text/markdown",
