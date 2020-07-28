0a3388b106644e8b61dab3b5541c67e50c3523a1
margulies <daniel.margulies@gmail.com>
2020-07-28 14:47:59 +0300

    updated requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-from setuptools import setup
+import setuptools 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 try:
     # for pip >= 10
     from pip._internal.req import parse_requirements
@@ -11,16 +14,18 @@ def load_requirements(fname):
     reqs = parse_requirements(fname, session="test")
     return [str(ir.req) for ir in reqs]
 
-setup(
+setuptools.setup(
   name = 'surfdist',
   packages = ['surfdist'],
-  version = '0.14.0',
+  version = '0.15.4',
   description = 'For calculating exact geodesic distances on cortical surface meshes',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Daniel Margulies',
   author_email = 'daniel.margulies@gmail.com',
   url = 'https://github.com/NeuroanatomyAndConnectivity/surfdist',
   keywords = ['geodesic', 'distance', 'brain', 'cortex'],
   license='LICENSE.txt',
-  long_description=open('README.md').read(),
+  python_requires='>=3.6',  
   install_requires=load_requirements("requirements.txt")
 )
