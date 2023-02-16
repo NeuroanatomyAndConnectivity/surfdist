fix distance calculation label loading

surfdist/analysis.py
@@ -1,8 +1,7 @@
 import gdist
 import numpy as np
 from surfdist.utils import surf_keep_cortex, translate_src, recort
-
-
+import surfdist as sd
 def dist_calc(surf, cortex, source_nodes):
 
     """
@@ -53,28 +52,33 @@ def dist_calc_matrix(surf, cortex, labels, exceptions = ['Unknown', 'Medial_wall
       dist_mat: symmetrical nxn matrix of minimum distance between pairs of labels
       rois: label names in order of n
     """
-
     cortex_vertices, cortex_triangles = surf_keep_cortex(surf, cortex)
 
     # remove exceptions from label list:
     label_list = sd.load.get_freesurfer_label(labels, verbose = False)
-    rs = np.where([a not in exceptions for a in label_list])[0]
-    rois = [label_list[r] for r in rs]
-    if verbose:
-        print("# of regions: " + str(len(rois)))
+    tmp=[i.decode('utf-8') for i in label_list]
+    label_list=tmp
+    del tmp
+    rois=list(set(label_list)-set(exceptions))
+    print(len(label_list))
+    print(len(exceptions))
+    print(len(rois))
 
-    # calculate distance from each region to all nodes:
+#     if verbose:
+#         print("# of regions: " + str(len(rois)))
+
+    ###calculate distance from each region to all nodes:
     dist_roi = []
     for roi in rois:
         source_nodes = sd.load.load_freesurfer_label(labels, roi)
         translated_source_nodes = translate_src(source_nodes, cortex)
         dist_roi.append(gdist.compute_gdist(cortex_vertices, cortex_triangles,
                                                 source_indices = translated_source_nodes))
-        if verbose:
-            print(roi)
+#         if verbose:
+#             print(roi)
     dist_roi = np.array(dist_roi)
 
-    # Calculate min distance per region:
+    ###Calculate min distance per region:
     dist_mat = []
     for roi in rois:
         source_nodes = sd.load.load_freesurfer_label(labels, roi)
@@ -82,4 +86,4 @@ def dist_calc_matrix(surf, cortex, labels, exceptions = ['Unknown', 'Medial_wall
         dist_mat.append(np.min(dist_roi[:,translated_source_nodes], axis = 1))
     dist_mat = np.array(dist_mat)
 
-    return dist_mat, rois
+    return dist_mat,rois